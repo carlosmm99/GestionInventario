29c8edbac82e102a42a8532e0049b429cd6f4805
carlosmm99 <carlosmondejar99@gmail.com>
2024-02-19 12:27:04 +0100

    Servlet para obtener equipos y fungibles

---
+++ AplicacionGestionInventario/src/java/controlador/ObtenerEquiposYFungibles.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controlador;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+import modelo.Equipo;
+import modelo.Fungible;
+
+/**
+ *
+ * @author carlos.mondejar
+ */
+public class ObtenerEquiposYFungibles extends HttpServlet {
+
+    private final Controlador c = new Controlador();
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        Gson gson = new Gson();
+
+        // Obtener los datos de equipos y fungibles del controlador
+        // Supongamos que obtienes estos datos de alguna manera
+        List<Equipo> equipos = c.leerEquipos();
+        List<Fungible> fungibles = c.leerFungibles();
+
+        // Convertir los datos a arrays
+        Equipo[] equiposArray = equipos.toArray(Equipo[]::new);
+        Fungible[] fungiblesArray = fungibles.toArray(Fungible[]::new);
+
+        // Construir un objeto JSON que contenga ambos arrays
+        JsonObject respuestaJson = new JsonObject();
+        respuestaJson.add("equipos", gson.toJsonTree(equiposArray));
+        respuestaJson.add("fungibles", gson.toJsonTree(fungiblesArray));
+
+        // Enviar la respuesta JSON al cliente
+        out.println(respuestaJson.toString());
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
