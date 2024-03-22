3ad091a6fb85d77dbb99bc1cec815667ff2fb00d
carlosmm99 <carlosmondejar99@gmail.com>
2024-03-22 13:21:01 +0100

    modificación URL título al pasar ratón por encima de notificación

--- AplicacionGestionInventario/web/js/gestionaPaginaInicio.js
+++ AplicacionGestionInventario/web/js/gestionaPaginaInicio.js
@@ -190,8 +190,12 @@ function crearAlertaCalibracion(equipo, color, mensaje) {
         var titleText = "";
 
         // Verificar si estamos en localhost
-        if (currentLocation.includes("localhost") || currentLocation.includes("127.0.0.1")) {
+        if (currentLocation.includes("localhost")) {
             titleText = "http://localhost:8080" + contexto + "/GestionEquipos";
+        } else if (currentLocation.includes("127.")) {
+            // Obtener la dirección IP del servidor
+            var ipAddress = currentLocation.match(/127\.\d{1,3}\.\d{1,3}\.\d{1,3}/)[0];
+            titleText = "http://" + ipAddress + contexto + "/GestionEquipos";
         } else {
             // Obtener la dirección IP del servidor
             var ipAddress = currentLocation.split('/')[2];
@@ -283,8 +287,12 @@ function crearAlertaCantidad(fungible, color, mensaje) {
         var titleText = "";
 
         // Verificar si estamos en localhost
-        if (currentLocation.includes("localhost") || currentLocation.includes("127.0.0.1")) {
+        if (currentLocation.includes("localhost")) {
             titleText = "http://localhost:8080" + contexto + "/GestionFungibles";
+        } else if (currentLocation.includes("127.")) {
+            // Obtener la dirección IP del servidor
+            var ipAddress = currentLocation.match(/127\.\d{1,3}\.\d{1,3}\.\d{1,3}/)[0];
+            titleText = "http://" + ipAddress + contexto + "/GestionFungibles";
         } else {
             // Obtener la dirección IP del servidor
             var ipAddress = currentLocation.split('/')[2];
@@ -376,8 +384,12 @@ function crearAlertaMantenimiento(equipo, color, mensaje) {
         var titleText = "";
 
         // Verificar si estamos en localhost
-        if (currentLocation.includes("localhost") || currentLocation.includes("127.0.0.1")) {
+        if (currentLocation.includes("localhost")) {
             titleText = "http://localhost:8080" + contexto + "/GestionEquipos";
+        } else if (currentLocation.includes("127.")) {
+            // Obtener la dirección IP del servidor
+            var ipAddress = currentLocation.match(/127\.\d{1,3}\.\d{1,3}\.\d{1,3}/)[0];
+            titleText = "http://" + ipAddress + contexto + "/GestionEquipos";
         } else {
             // Obtener la dirección IP del servidor
             var ipAddress = currentLocation.split('/')[2];
